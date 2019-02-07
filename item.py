dfff8f5b2b7b4205f12d258de23ff5590552aa60
Lazaro Franco <lazaro300x@icloud.com>
2019-02-07 16:32:59 -0500

    Almost done

--- item.py
+++ item.py
@@ -1,5 +1,15 @@
 import sys
+import datetime
+# A timestamp of when they were created DONE
+# A boolean marking the item as completed or not DONE
+# Text of the actual to-do item DONE
 
-# A timestamp of when they were created
-# A boolean marking the item as completed or not
-# Text of the actual to-do item
+class Item(object):
+
+    def __init__(self):
+        self.timestamp = datetime.datetime.now()
+        self.boolean = True or False
+        self.task = task
+
+
+Item()
--- todos.py
+++ todos.py
@@ -1,49 +1,6 @@
 import os.path
+from manager import Manager
 
-todos = open('todos.txt', 'r')
-
-
-def start():
-
-    print("Choose a day you will like to make a plan.")
-    choice = input("")
-    if choice == "Monday":
-        todos.read()
-    elif choice == "Tuesday":
-        todos.read()
-    elif choice == "Wednesday":
-        todos.read()
-    elif choice == "Thursday":
-        todos.read()
-    elif choice == "Friday":
-        todos.read()
-    elif choice == "Saturday":
-        todos.read()
-    elif choice == "Sunday":
-        todos.read()
-    else:
-        print("Incorrect spelling")
-        start()
-
-    print()
-    todo = open('todos.txt', 'a+')
-    print("Type in your new plan! ")
-    #Getting What To Write To File
-    text = input("")
-    #Actually Writing It
-    todo.write(f"{choice}: {text} \n")
-    todo.close()
-
-def read():
-    print()
-    print("These are your plans for the week: ")
-    todos = ('todos.txt')
-    if os.path.isfile(todos):
-        todos = open('todos.txt', 'r')
-        print(todos.read())
-    else:
-        print("Error")
-        start()
-
+app = Manager()
 start()
 read()
